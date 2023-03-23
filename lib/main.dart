1554706d29c14c112d5a9b7f98bb07064d995973
John <yantraparti.johnravikanth@isowhiz.in>
2023-03-23 12:57:38 +0530

    Dynamic padding for scrollview placed behind bottom view in a stack

--- lib/main.dart
+++ lib/main.dart
@@ -95,11 +95,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   width: double.infinity,
                   height: 700,
                   color: Colors.orange,
-                ),
-                Container(
-                  width: double.infinity,
-                  height: doubleHeight,
-                  color: Colors.limeAccent,
+                  margin: EdgeInsets.only(bottom: doubleHeight),
                 ),
               ],
             ),
@@ -121,7 +117,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 });
                 return Container(
                   width: MediaQuery.of(context).size.width,
-                  color: Colors.transparent.withOpacity(0.1),
+                  color: Colors.grey,
                   padding: const EdgeInsets.all(8),
                   child: const Text(
                     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
